11196f5a16df6fd8bf2ce339bbe48b0255f694f1
Tylerdays <tylerdday.software@gmail.com>
2023-01-25 23:27:26 -0700

    Just added the python file

---
+++ python/fizzbang.py
@@ -0,0 +1,12 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for n in numbers:
+    fizzbuzz(n)
